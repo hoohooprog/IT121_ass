49ae1d850fbab00c1edc47c8bd06be170d67654a
Tay <tyjingyi@gmail.com>
2022-01-26 15:57:05 -0800

    use of both template literal and concatenation for assignment 2

---
+++ wk2/index_w2.js
@@ -0,0 +1,15 @@
+var num1;
+var num2;
+var total;
+ 
+num1 = 5;
+num2 = 7;
+total = num1 + num2;
+
+let result = `The sum of ${num1} + ${num2} is : ${total}`;
+
+document.write("Welcome to my calculator"+ "</br>");
+document.write("The sum of " + num1 + " + " + num2 + " is : " + total + "</br>");
+document.write(result);
+document.write("</br>");
+document.write(result);
