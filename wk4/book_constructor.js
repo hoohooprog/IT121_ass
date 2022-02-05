50997f18588b295eab44973f9f15a16377e9e208
hoohooprog <tyjingyi@gmail.com>
2022-02-05 14:19:56 -0800

    updated comments
    included what additions would be great such as user input, confirming and storing data for interactivity, coding and familiarity purposes.

--- wk4/book_constructor.js
+++ wk4/book_constructor.js
@@ -2,6 +2,8 @@
     either a Book object or comment object, can be expanded to include Author object
     that has a collection of books as array or JSON in  a Author object
 */
+
+// Book function template has properties that record the title, author, genre and summary as string objects
 function Book(title, author, genre, summary){
 
     this.title = title;
@@ -27,13 +29,17 @@ function Book(title, author, genre, summary){
 
 }
 
+// would be nice to create real prompt and exceptionhandling to make sure user entered all the fields correctly
+
+// creates 2 new book objects for testing
 let book1 = new Book("Constantine", "frusciante", "funk horror", "ABC,123 etc");
 let book2 = new Book("Liposuction: The definitive guide","Guru K", "medical","XYZ,123 etc");
 
+// assigns element ids to their respective variables
 let el_bookTitle = document.getElementById('titles_available');
 let el_bookGenre = document.getElementById('genres_available');
 
-/* display current collection of Titles and Genre, next iteration to write in by array */
+/* display current collection of Titles and Genre in HTML, next iteration to write in by array */
 
 el_bookTitle.innerHTML = `${book1.retTitle()}, author: ${book1.author}, summary: ${book1.summary}` + "</br>" 
             + `${book2.retTitle()}, author: ${book2.author}, summary: ${book1.summary}`;
