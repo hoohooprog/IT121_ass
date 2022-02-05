5fb27c00c20848e9a522bda447e393de364e70ea
hoohooprog <tyjingyi@gmail.com>
2022-02-05 14:13:17 -0800

    added comments
    understanding of prompt fun , template literals and parseInt()

--- wk3/index_w3.js
+++ wk3/index_w3.js
@@ -10,10 +10,12 @@ let total;
 let user_name;
  
 // tells user what game is going on, followed by a new line asking for his name
+// prompts return string values input by user
 user_name = prompt("Let's play the sum game.\nPlease enter your name");
 num1 = prompt("Enter first number");
 num2 = prompt("Enter second number");
 
+// parseInt helps to convert string to int objects to be summed and returned in template literal as strings
 document.write(`Hi, ${user_name}! The sum of ${num1} + ${num2} is ${(parseInt(num1) + parseInt(num2))}`);
 
 
