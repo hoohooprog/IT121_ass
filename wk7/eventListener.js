408a1476b27882764a327c2b4a5a539a8b96e321
Tay <tyjingyi@gmail.com>
2022-03-18 00:45:09 -0700

    done with html, might change to forms for output, js pseudo-coded, to be implemented

---
+++ wk7/eventListener.js
@@ -0,0 +1,21 @@
+// https://stackoverflow.com/questions/2874688/how-to-disable-an-input-type-text
+
+
+document.getElementById('MPG').disabled = true;
+
+/*  activates and displays value in MPG output box when user presses
+   submit button; hence eventlistener is required to target button and
+   callback fn when click event is applied to button*/
+
+submitBut = document.querySelector('button[type="submit"]');
+
+
+
+submitBut.addEventListener('click', function(){
+    document.getElementById('MPG').disabled = false;
+    var MilesDrive = document.getElementById('milesDriven');
+    var gasGallonUsed = document.getElementById('gasGallonUsed');
+    var MPG = document.getElementById('MPG');
+    MPG.value = MilesDriven.value / gasGallonUsed.value;
+  } );
+
